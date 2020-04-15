27268acd57c8e0d79a7c864816fcf4d63936e5b4
yutao <862238585@qq.com>
2020-04-15 10:41:51 +0800

    add base template

--- app.py
+++ app.py
@@ -66,9 +66,19 @@ class Movie(db.Model):
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
 
 
 if __name__ == '__main__':
